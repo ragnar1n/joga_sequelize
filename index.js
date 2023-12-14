1793098b51d2dc2ae0d852365f250172cd071480
Ragnar <ragnar.naruski@voco.ee>
2023-12-14 21:48:23 +0200

    Created new article

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const articleRouter = require('./routes/article')
 app.use('/', articleRouter)
 app.use('/article', articleRouter)
 app.use('/author', articleRouter)
+app.use('/admin/article', articleRouter)
 
 app.listen(3000,() => {
 
--- routes/article.js
+++ routes/article.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const articleController = require('../controllers/article')
 const authorController = require('../controllers/author')
+const articleAdminController = require('../controllers/admin/article')
 
 router.get('/', articleController.getAllArticles)
 router.get('/article/:slug', articleController.getArticleBySlug)
 router.get('/author/:id', authorController.getAuthorArticles)
+router.post('/admin/article/create', articleAdminController.createArticle)
 
 module.exports = router
